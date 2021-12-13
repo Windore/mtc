c04c41cfb09fcd5e630d2d426ae5b6953070953c
Windore <windore.wi@protonmail.com>
2021-12-14 00:40:55 +0200

    Added the Add command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use mtc::*;
-use std::path::Path;
-use std::io::{BufReader, BufWriter, Write};
-use std::{fs, fs::File};
-use serde::{Serialize, de::DeserializeOwned};
+use serde::{de::DeserializeOwned, Serialize};
+use std::env;
 use std::fmt::Display;
+use std::str::FromStr;
+use std::io::{self, BufReader, BufWriter, Write};
+use std::path::Path;
+use std::{fs, fs::File};
 
 pub struct Items {
     pub todo_items: MtcList<TodoItem>,
@@ -15,65 +17,280 @@ mod commands {
     use super::*;
     use chrono::prelude::*;
 
-    const weekdays: &[Weekday] = &[Weekday::Mon, Weekday::Tue, Weekday::Wed, Weekday::Thu, Weekday::Fri, Weekday::Sat, Weekday::Sun];
+    pub fn handle_command(mut items: Items) -> Items {
+        // There probably is a better way to do this. However the overhead is insignificant so it doesn't matter that much.
+        let args: Vec<String> = env::args().collect();
+        let mut args = args.iter().map(|s| s.as_str());
 
-    pub fn handle_command(items: Items) -> Items {
+        args.next();
+
+        match args.next() {
+            Some("show") => show_cmd::show(&items, args),
+            Some("help") => help(),
+            Some("add") => add_move_cmd::add(&mut items, args),
+            None => {
+                eprintln!("Not enough arguments.");
+                tip();
+            },
+            _ => {
+                eprintln!("Unknown command.");
+                tip();
+            },         
+        }
+
         items
     }
 
-    fn show_all(items: Items) {
-        for wd in weekdays.iter() {
+    fn help() {
+        println!("TODO: Add help");
+    }
 
+    fn tip() {
+        println!("Use: 'mtc help' for help.");
+    }
+
+    mod add_move_cmd {
+        use super::*;
+
+        pub fn add<'a, T>(items: &mut Items, mut args: T) where T: Iterator<Item = &'a str> {
+            match args.next() {
+                Some("todo-item") => add_todo_item(items),
+                Some("task") => add_task(items),
+                Some("event") => add_event(items),
+                Some(typ) => {
+                    eprintln!("Unknown type: '{}'", typ);
+                    tip();
+                }
+                None => {
+                    eprintln!("No type specified.");
+                    tip();
+                }
+            } 
+        }
+
+        fn add_todo_item(items: &mut Items) {
+            println!("New todo-item: ");
+            let body = read_body();
+            let weekday = read_weekday();
+            items.todo_items.add(TodoItem::new(body, weekday))
+        }
+
+        fn add_task(items: &mut Items) {
+            println!("New task: ");
+            let body = read_body();
+            let duration = read_duration();
+            let weekday = read_weekday();
+            items.tasks.add(Task::new(body, duration, weekday))
+        }
+
+        fn add_event(items: &mut Items) {
+            println!("New event: ");
+            let body = read_body();
+            let date = read_date();
+            items.events.add(Event::new(body, date))
+        }
+
+        fn read_weekday() -> Option<Weekday> {
+            loop {
+                print!("Input a weekday (empty for none): ");
+                io::stdout().flush().expect("Failed to flush stdout.");
+                let mut inp = String::new();
+                io::stdin().read_line(&mut inp).expect("Failed to read stdin.");
+                inp = inp.replace('\n', "");
+
+                if inp.trim().len() == 0 {
+                return None; 
+                }
+
+                match Weekday::from_str(&inp) {
+                    Ok(day) => return Some(day),
+                    Err(_) => {
+                        eprintln!("Cannot parse '{}' to a weekday.", &inp);
+                    }
+                }
+            }
+        }
+
+        fn read_duration() -> u32 {
+            loop {
+                print!("Input a duration in minutes: ");
+                io::stdout().flush().expect("Failed to flush stdout.");
+                let mut inp = String::new();
+                io::stdin().read_line(&mut inp).expect("Failed to read stdin.");
+                inp = inp.replace('\n', "");
+
+                match u32::from_str(&inp) {
+                    Ok(n) => return n,
+                    Err(_) => {
+                        eprintln!("Cannot parse '{}' to a number.", &inp);
+                    }
+                }
+            }
+        }
+
+        fn read_body() -> String {
+            loop {
+                print!("Input a body: ");
+                io::stdout().flush().expect("Failed to flush stdout.");
+                let mut inp = String::new();
+                io::stdin().read_line(&mut inp).expect("Failed to read stdin.");
+                inp = inp.replace('\n', "");
+
+                return inp;
+            }
+        }
+
+        fn read_date() -> NaiveDate {
+            loop {
+                print!("Input a date (yyyy-mm-dd): ");
+                io::stdout().flush().expect("Failed to flush stdout.");
+                let mut inp = String::new();
+                io::stdin().read_line(&mut inp).expect("Failed to read stdin.");
+                inp = inp.replace('\n', "");
+
+                match NaiveDate::from_str(&inp) {
+                    Ok(date) => return date,
+                    Err(_) => {
+                        eprintln!("Cannot parse '{}' to a date.", &inp);
+                    }
+                }
+            }
         }
     }
 
-    fn show_weekday(weekday: Weekday) {
-       
-    }
+    mod show_cmd {
+        use super::*;
 
-    fn show_today() {
+        const WEEKDAYS: &[Weekday] = &[
+            Weekday::Mon,
+            Weekday::Tue,
+            Weekday::Wed,
+            Weekday::Thu,
+            Weekday::Fri,
+            Weekday::Sat,
+            Weekday::Sun,
+        ];
 
-    }
-
-    fn show_week(items: &Items) {
-        let mut day = Local::today();
-        let orig_day = day.weekday();
-
-        while day.succ().weekday() != orig_day {
-            show_items_date(&items.todo_items, day.naive_local());
-            show_items_date(&items.tasks, day.naive_local());
-            show_items_date(&items.events, day.naive_local());
-            day = day.succ();
+        pub fn show<'a, T>(items: &Items, mut args: T) where T: Iterator<Item = &'a str> {
+            match args.next() {
+                Some("todo-items") => show_all_todo_items(items),
+                Some("tasks") => show_all_tasks(items),
+                Some("events") => show_all_events(items),
+                Some("today") => show_today(items),
+                Some("week") => show_week(items),
+                Some("month") => show_month(items),
+                Some(weekday) => {
+                    if let Ok(wd) = weekday.parse::<Weekday>() {
+                        show_weekday(items, wd);
+                    } else {
+                        eprintln!("Cannot parse '{}' to a weekday.", weekday);
+                        tip();
+                    }
+                }
+                None => show_all(items),
+            }
         }
-    }
 
-    fn show_items_weekday<T: MtcItem + Clone + Ord + Display>(list: &MtcList<T>, weekday: Weekday) {
-        let mut items_vec = list.items_for_weekday(weekday);
-        items_vec.sort();
-        show_list(&items_vec);
-    }
 
-    fn show_items_date<T: MtcItem + Clone + Ord + Display>(list: &MtcList<T>, date: NaiveDate) {
-        let mut items_vec = list.items_for_date(date);
-        items_vec.sort();
-        show_list(&items_vec);
-    }
+        fn show_all(items: &Items) {
+            for wd in WEEKDAYS.iter() {
+                println!("{}", wd);
+                println!("\tTodo-Items: ");
+                show_list_weekday(&items.todo_items, *wd);
+                println!("");
+                println!("\tTasks: ");
+                show_list_weekday(&items.tasks, *wd);
+                println!("");
+            }
+            show_all_events(&items);
+        }
 
-    fn show_items_today<T: MtcItem + Clone + Ord + Display>(list: &MtcList<T>) {
-        let mut items_vec = list.items_for_today();
-        items_vec.sort();
-        show_list(&items_vec);
-    }
+        fn show_weekday(items: &Items, weekday: Weekday) {
+            let mut date = Local::today();
+            while date.weekday() != weekday {
+                date = date.succ();
+            }
+            show_all_date(&items, date.naive_local());
+        }
 
-    fn show_all_events(items: &Items) {
-        let mut events_vec = items.events.items();
-        events_vec.sort();
-        show_list(&events_vec);
-    }
+        fn show_today(items: &Items) {
+            let day = Local::today();
+            show_all_date(items, day.naive_local());
+        }
 
-    fn show_list<T: Display>(list: &[&T]) {
-        for i in list.iter() {
-            println!("\t{}", i);
+        fn show_week(items: &Items) {
+            let mut day = Local::today().naive_local();
+            let orig_day = day.weekday();
+
+            while day.succ().weekday() != orig_day {
+                show_all_date(items, day);
+                day = day.succ();
+            }
+        }
+
+        fn show_month(items: &Items) {
+            let mut day = Local::today().naive_local();
+
+            for _ in 0..30 {
+                show_all_date(items, day);
+                day = day.succ();
+            }
+        }
+
+        fn show_all_todo_items(items: &Items) {
+            for wd in WEEKDAYS.iter() {
+                println!("{}", wd);
+                println!("\tTodo-Items: ");
+                show_list_weekday(&items.todo_items, *wd);
+            }
+        }
+
+        fn show_all_tasks(items: &Items) {
+            for wd in WEEKDAYS.iter() {
+                println!("{}", wd);
+                println!("\tTasks: ");
+                show_list_weekday(&items.tasks, *wd);
+            }
+        }
+
+        fn show_all_events(items: &Items) {
+            println!("Events: ");
+            let mut events_vec = items.events.items();
+            events_vec.sort();
+            for i in events_vec.iter() {
+                println!("\t{}", i);
+            }
+        }
+
+        fn show_all_date(items: &Items, date: NaiveDate) {
+            println!("{} {}:", date.weekday(), date);
+            println!("\tTodo-Items: ");
+            show_list_date(&items.todo_items, date);
+
+            println!("\tTasks: ");
+            show_list_date(&items.tasks, date);
+
+            println!("\tEvents: ");
+            show_list_date(&items.events, date);
+        }
+
+
+        fn show_list_date<T: MtcItem + Clone + Ord + Display>(list: &MtcList<T>, date: NaiveDate) {
+            let mut items_vec = list.items_for_date(date);
+            items_vec.sort();
+            show_list(&items_vec);
+        }
+
+        fn show_list_weekday<T: MtcItem + Clone + Ord + Display>(list: &MtcList<T>, weekday: Weekday) {
+            let mut items_vec = list.items_for_weekday(weekday);
+            items_vec.sort();
+            show_list(&items_vec);
+        }
+
+        fn show_list<T: Display>(list: &[&T]) {
+            for i in list.iter() {
+                println!("\t\t{}", i);
+            }
         }
     }
 }
@@ -93,7 +310,7 @@ fn main() {
                 eprintln!("Reading saved items failed.");
                 eprintln!("{}", msg);
                 return;
-            } 
+            }
         };
 
         if let Err(msg) = write_items(&dir, modified_items) {
@@ -115,13 +332,11 @@ fn read_items(dir: &Path) -> Result<Items, String> {
     let tasks = read_item(&task_file)?;
     let events = read_item(&event_file)?;
 
-    Ok(
-        Items {
-            todo_items,
-            tasks,
-            events,
-        }
-    )
+    Ok(Items {
+        todo_items,
+        tasks,
+        events,
+    })
 }
 
 fn read_item<T: MtcItem + Clone + DeserializeOwned>(path: &Path) -> Result<MtcList<T>, String> {
@@ -147,7 +362,10 @@ fn write_items(dir: &Path, items: Items) -> Result<(), String> {
     Ok(())
 }
 
-fn write_item<T: MtcItem + Clone + DeserializeOwned + Serialize>(item: MtcList<T>, path: &Path) -> Result<(), String> {
+fn write_item<T: MtcItem + Clone + DeserializeOwned + Serialize>(
+    item: MtcList<T>,
+    path: &Path,
+) -> Result<(), String> {
     let file = File::create(path).map_err(|e| e.to_string())?;
     let mut writer = BufWriter::new(file);
     serde_json::to_writer_pretty(&mut writer, &item).map_err(|e| e.to_string())?;
